45477f30a49c816c92f326a5de24bed45a74305d
root <root@localhost.localdomain>
2018-12-10 09:20:49 -0500

    Adding System Introspection(Step 4)

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,5 +1,11 @@
+#include "TutorialConfig.h"
 #include <math.h>
+#include <stdio.h>
 
 double mysqrt(double v) {
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+    return exp(log(v)*0.5);
+#else
     return sqrt(v);
+#endif
 }
